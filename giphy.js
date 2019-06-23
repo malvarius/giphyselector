abca881add256cb1077782b048299d9f60f2d08f
Eric Malvar <47836797+malvarius@users.noreply.github.com>
2019-06-23 01:22:29 -0700

    added gif functionality still having issues once button is added

--- giphy.js
+++ giphy.js
@@ -4,38 +4,48 @@ var gameArray = ['Zelda', 'Donkey Kong', 'World of Warcraft', 'Super Mario'];
 // function to add buttons based on gameArray titles for each element
 var displayArray = function (array) {
     // first empties button div
-    $('.btnContain').empty();
     // creates button for each element in array
     for (i = 0; i < array.length; i++) {
-        var but = $("<button class='btn btn-outline-secondary mr-2' type='button'></button>").attr('game-name', gameArray[i]);
+        var but = $("<button class='btn gifBtn btn-outline-secondary mr-2 type='button'></button>");
+        but.attr("gif-name",array[i]);
         but.text(array[i])
         $('.btnContain').append(but);
     }
 }
 displayArray(gameArray);
 
-// on click function to push text from search bar to gameArray
+// on click function to push text from search bar to gameArray and regenerate buttons
 
 $('.searchBtn').on('click', function (event) {
     event.preventDefault();
+    $('.btnContain').empty();
     const input = $('.userIn').val();
     gameArray.push(input);
     displayArray(gameArray);
     $('.userIn').val("");
 })
 
-// axios command 
-var link = "https://api.giphy.com/v1/gifs/search?api_key=mcTa9s1YDz1zMg4Ad6oszrHFMV71V7oo&q=dog&limit=10&offset=0&rating=PG&lang=en";
+// axios command  to get gifs based on gif-name attribute , gets 10 gifs
+$('.gifBtn').on('click',function(){
+    console.log($(this).attr('gif-name'))
+    $('.gifContain').empty();
+    const search = $(this).attr('gif-name');
+    const link = "https://api.giphy.com/v1/gifs/search?api_key=mcTa9s1YDz1zMg4Ad6oszrHFMV71V7oo&q="+search+"&limit=10&offset=0&rating=PG&lang=en";
 axios({ url: link,
         method: "GET",
 })
   .then(function (response) {
-    const giphy = JSON.stringify(response.data.data[0].images.fixed_width_small.url);
-    const pic = $("<img src="+giphy+" class='gif' alt='blank'>");
+    console.log(response.data.data);
+      for(i=0;i<10;i++){
+    var gip = JSON.stringify(response.data.data[i].images.downsized_medium.url);
+    const pic = $("<img src="+gip+" class='mt-4 mr-3 gif' alt='blank'>");
     $('.gifContain').append(pic);
-    console.log(giphy);
+      }
+      
   })
   .catch(function (error) {
     // handle error
     console.log(error);
   })
+
+})
